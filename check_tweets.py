38ff5970d3c7b3d1c22141109750b4cf34248656
boycreed4-star <boycreed4@gmail.com>
2026-09-15 07:08:56 +0100

    Update Telegram targets in check_tweets.py

--- check_tweets.py
+++ check_tweets.py
@@ -32,7 +32,7 @@ from twikit import Client
 # ---- Configuration -------------------------------------------------------
 
 TWITTER_HANDLES = ["BarcaTimes"]  # no @, add more here later
-TELEGRAM_TARGETS = ["@footbal_2325", "@fcbarcelonachatgroup"]  # channel + group
+TELEGRAM_TARGETS = ["@barcatimes1", "@fcbarcelonachatgroup"]  # channel + group
 COOKIES_FILE = "cookies.json"
 MAX_TWEETS_PER_RUN = 5                 # safety cap so a big backlog can't spam the channel
 
